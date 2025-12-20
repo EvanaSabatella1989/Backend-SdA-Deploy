29e1cb858d61bf07aeeb12176a96f49c2120d774
Margarita Bravo <embravo3329@gmail.com>
2025-12-20 12:22:27 -0300

    editando serializer venta

--- back/venta/serializer.py
+++ back/venta/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Venta
 
 class VentaSerializer(serializers.ModelSerializer):
+    cliente = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Venta
         fields = '__all__'
