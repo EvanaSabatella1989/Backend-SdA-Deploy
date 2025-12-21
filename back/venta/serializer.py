1832d408fd5f8e9b8175a50f575cbdaad5029b11
Margarita Bravo <embravo3329@gmail.com>
2025-12-21 14:33:06 -0300

    corrigiendo error

--- back/venta/serializer.py
+++ back/venta/serializer.py
@@ -2,12 +2,6 @@ from rest_framework import serializers
 from .models import Venta
 from venta_detalle.models import VentaDetalle
 
-class VentaSerializer(serializers.ModelSerializer):
-    cliente = serializers.PrimaryKeyRelatedField(read_only=True)
-    class Meta:
-        model = Venta
-        fields = '__all__'
-        
 class VentaDetalleSerializer(serializers.ModelSerializer):
     producto_nombre = serializers.CharField(source='producto.nombre', read_only=True)
 
@@ -20,3 +14,15 @@ class VentaDetalleSerializer(serializers.ModelSerializer):
             'descuento'
         ]
 
+
+class VentaSerializer(serializers.ModelSerializer):
+    cliente = serializers.PrimaryKeyRelatedField(read_only=True)
+    detalles = VentaDetalleSerializer(
+        source='ventadetalle_set',
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Venta
+        fields = '__all__'
