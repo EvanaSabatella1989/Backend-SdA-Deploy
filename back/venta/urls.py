42e955098845481e4a62b3fb814d62fd8131ddf9
EvanaSabatella1989 <eva.sabatella@gmail.com>
2026-01-04 11:17:39 -0300

    urls venta/confirmar-pago prueba 2

--- back/venta/urls.py
+++ back/venta/urls.py
@@ -5,7 +5,7 @@ from .views import ReferenceMPView, ConfirmarPagoView,VentaDetailView,VentaListV
 
 urlpatterns = [
     path('venta/preference/', ReferenceMPView.as_view()),
-    path("venta/confirmar-pago/", ConfirmarPagoView.as_view(), name="confirmar-pago"),
+    path('venta/confirmar-pago/', ConfirmarPagoView.as_view(), name='confirmar-pago'),
     path('ventas/', VentaListView.as_view(), name='ventas'),
     path('ventas/<int:pk>/', VentaDetailView.as_view(), name='venta-detalle'),
 ]
